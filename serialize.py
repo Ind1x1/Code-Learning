bac86157989789602b5e962991cd6102ea6f3434
Ind1ex1 <2777904242@qq.com>
2025-01-20 13:44:36 +0800

    first code

---
+++ serialize.py
@@ -0,0 +1,43 @@
+# Copyright CODE LEARNING (alpha) 2024 Leyi Ye
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+from dataclasses import dataclass, field
+from typing import Dict
+
+
+# transmit to dict 
+def to_dict(o):
+    if hasattr(o, "to_dict"):
+        return o.to_dict()
+    elif hasattr(o, "__dict__"):
+        return o.__dict__
+    else:
+        return {}
+
+# transmit python class to json Import
+class JsonSerializable(object):
+    def to_json(self, indent=None):
+        return json.dumps(
+            self,
+            default=to_dict,
+            sort_keys=True,
+            indent=indent,
+        )
+
+
+@dataclass
+class ClassMeta:
+    module_path: str = ""
+    class_name: str = ""
+    kwargs: Dict[str, str] = field(default_factory=dict)
